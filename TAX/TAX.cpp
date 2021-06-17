d7b8602fcdab0ccbff2e184b43529f886014fda1
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-17 21:11:50 +0300

    Add project files.

---
+++ TAX/TAX.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+void come() {
+	double income, tax = 0;
+	cout << "enter you income : ";
+	do {
+		cin >> income;
+		if (income >= 35000) {
+			tax = income * 0.2;
+			income -= tax;
+			cout << income << " income after taxes(20%)\a" << endl;
+		}
+		else if (income >= 20000) {
+			tax = income * 0.15;
+			income -= tax;
+			cout << income << " income after taxes(15%)\a" << endl;
+		}
+		else if (income >= 10000) {
+			tax = income * 0.1;
+			income -= tax;
+			cout << income << " income after taxes(10%)\a" << endl;
+		}
+		else
+			cout << income << " income with no taxes\a" << endl;
+
+
+	} while (income != 0);
+	cout << "no income\a";
+}
+int main() {
+	come();
+}
