e6f842b7e80542a9dce41cb37b5d95091ba96357
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-09-02 19:27:16 +0300

    Update TAX.cpp

--- TAX/TAX.cpp
+++ TAX/TAX.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 
 using namespace std;
-//this program dose a simple calculating for taxes.
+//this program dose a simple calculating for taxes
 
 void tax() {
-	system("color 4");//set screen colour to RED.
+	system("color 4");//set screen colour to RED
 	double income, tax = 0;
 	cout << "enter you income : ";
-	do {//loop that keep asking for input till the user break it.
+	do {//loop that keep asking for input till the user break it
 		cin >> income;//get input
 		if (income >= 35000) {
-			tax = income * 0.2;//caculating the income with taxes.
+			tax = income * 0.2;//caculating the income with taxes
 			income -= tax;
 			cout << income << " income after taxes(20%)\a" << endl;
 		}
@@ -28,9 +28,9 @@ void tax() {
 			cout << income << " income with no taxes\a" << endl;
 
 
-	} while (income != 0);//loop breaks when input 0.
+	} while (income != 0);//loop breaks when input 0
 	cout << "no income\a";
 }
 int main() {
-	tax();//invoke the function.
+	tax();//invoke the function
 }
